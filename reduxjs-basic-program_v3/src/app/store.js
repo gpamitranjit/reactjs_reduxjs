356448036d68c21994e4b0ef403d983154f70407
Amit Patil (Krishna) <gpamitranjit@gmail.com>
2018-02-09 14:41:40 +0530

    reduxjs basic app being refactored
    for more proper structural manger.

---
+++ reduxjs-basic-program_v3/src/app/store.js
@@ -0,0 +1,13 @@
+import {createStore, combineReducers, applyMiddleware} from "redux";
+import { createLogger} from "redux-logger";
+import user from "./reducers/userReducer";
+import math from "./reducers/mathReducer";
+
+
+export default createStore(combineReducers({
+     math,
+     user
+    }),
+    {},
+    applyMiddleware(createLogger())
+    );
