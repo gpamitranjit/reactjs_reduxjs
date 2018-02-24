7cb508764658b076795ebdb0d1075e7189ab5fc6
Amit Patil <gpamitranjit@gmail.com>
2018-02-24 13:35:35 +0530

    Adding the comments on when to use array indexes as keys for the rendered list of items!!

--- Simple_reactjs_project_24/src/app/App.js
+++ Simple_reactjs_project_24/src/app/App.js
@@ -8,6 +8,7 @@ class App extends Component {
     super(props);
   }
 
+// use  f array indices as keys for the rendered list of items is Not recommended when the order of the list items changes it has negative impact on the react component state updation!!
   render() {
     const numbers = this.props.numbers;
     const doubledValues = numbers.map((number, index) => { 
